2e103db50dac43f7af046acbedf0729311ccdeaf
alipiapia <124910168@qq.com>
2016-01-02 20:36:07 +0800

    Update config

--- Bug/Common/Conf/config.php
+++ Bug/Common/Conf/config.php
@@ -4,14 +4,6 @@ return array(
 
 	// 数据库配置
 
-    // 'DB_TYPE'   => 'mysql', // 数据库类型
-    // 'DB_HOST'   => 'hvaornp0.zzcdb.dnstoo.com', // 服务器地址
-    // 'DB_NAME'   => 'wodeshujuku', // 数据库名
-    // 'DB_USER'   => 'wodeshujuku_f', // 用户名
-    // 'DB_PWD'    => 'wodemima',  // 密码
-    // 'DB_PORT'   => '4001', // 端口
-    // 'DB_PREFIX' => 'bug_', // 数据库表前缀
-
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'wodeshujuku', // 数据库名
