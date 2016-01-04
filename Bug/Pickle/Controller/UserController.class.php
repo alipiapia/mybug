e547014d1bf35f02f658b663729f6c5503c8a1c8
alipiapia <124910168@qq.com>
2016-01-04 12:27:30 +0800

    Init.2016

---
+++ Bug/Pickle/Controller/UserController.class.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pickle\Controller;
+use Think\Controller;
+
+class UserController extends Controller{
+	public function index(){
+		if(IS_POST){
+			$post_data = I('post.');
+			// p($post);
+			if($post_data['getform'] == 'login'){
+				$this->login($post_data);
+			}elseif($post_data['getform'] == 'register'){
+				$this->register($post_data);
+			}elseif($post_data['getform'] == 'forgetPassword'){
+				$this->forgetPassword($post_data);
+			}
+		}else{
+			$this->display();
+		}
+	}
+
+	public function login($post_data){
+		extract($post_data);
+		// p($post_data);
+		$user_data = M("User")->find();
+		// p($user_data);
+		// p($user_data['password']."===".md5($password));
+		if($username == $user_data['username'] && md5($password) == $user_data['password']){
+			$session_time = ($remember != '1') ? 3600 : 3600*24;
+			session(array('user' => $user_data, 'expire' => $session_time));
+			// p(session('user'));
+			$this->success("登錄成功！",U('Index/index'));
+		}else{
+			$this->error("賬號或密碼錯誤！");
+		}
+	}
+
+	public function register($post_data){
+		extract($post_data);
+		// p($post_data);
+	}
+
+	public function forgetPassword($post_data){
+		extract($post_data);
+		// p($post_data);
+	}
+
+	public function logOut(){
+		//
+	}
+}
