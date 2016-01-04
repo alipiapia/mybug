042c64ece00be12b8d4d184a7fc942e3f08eec8e
alipiapia <124910168@qq.com>
2016-01-04 14:09:05 +0800

    Update config.php

--- Bug/Common/Conf/config.php
+++ Bug/Common/Conf/config.php
@@ -4,21 +4,21 @@ return array(
 
 	// 数据库配置
 
-    // 'DB_TYPE'   => 'mysql', // 数据库类型
-    // 'DB_HOST'   => 'hvaornp0.zzcdb.dnstoo.com', // 服务器地址
-    // 'DB_NAME'   => 'wodeshujuku', // 数据库名
-    // 'DB_USER'   => 'wodeshujuku_f', // 用户名
-    // 'DB_PWD'    => 'wodemima',  // 密码
-    // 'DB_PORT'   => '4001', // 端口
-    // 'DB_PREFIX' => 'bug_', // 数据库表前缀
-
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_HOST'   => 'hvaornp0.zzcdb.dnstoo.com', // 服务器地址
     'DB_NAME'   => 'wodeshujuku', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
-    'DB_PORT'   => '3306', // 端口
+    'DB_USER'   => 'wodeshujuku_f', // 用户名
+    'DB_PWD'    => 'wodemima',  // 密码
+    'DB_PORT'   => '4001', // 端口
     'DB_PREFIX' => 'bug_', // 数据库表前缀
+
+    // 'DB_TYPE'   => 'mysql', // 数据库类型
+    // 'DB_HOST'   => 'localhost', // 服务器地址
+    // 'DB_NAME'   => 'wodeshujuku', // 数据库名
+    // 'DB_USER'   => 'root', // 用户名
+    // 'DB_PWD'    => '',  // 密码
+    // 'DB_PORT'   => '3306', // 端口
+    // 'DB_PREFIX' => 'bug_', // 数据库表前缀
 
 	// 'TMPL_PARSE_STRING'  =>array(
 	// 	'__PUBLIC__' => '/assets', // 更改默认的/Public 替换规则
@@ -27,6 +27,6 @@ return array(
 	// ),
 
 	'DEFAULT_MODULE' => 'Pickle',
-	// 'SHOW_PAGE_TRACE' => false,
+	'SHOW_PAGE_TRACE' => false,
 
 );
