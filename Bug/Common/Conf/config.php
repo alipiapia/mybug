1e1e4a74fef97c0a9ebb2ad2dd1173004edca027
alipiapia <124910168@qq.com>
2016-01-04 13:36:24 +0800

    Add config.php

---
+++ Bug/Common/Conf/config.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+	// 数据库配置
+
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'hvaornp0.zzcdb.dnstoo.com', // 服务器地址
+    'DB_NAME'   => 'wodeshujuku', // 数据库名
+    'DB_USER'   => 'wodeshujuku_f', // 用户名
+    'DB_PWD'    => 'wodemima',  // 密码
+    'DB_PORT'   => '4001', // 端口
+    'DB_PREFIX' => 'bug_', // 数据库表前缀
+
+    // 'DB_TYPE'   => 'mysql', // 数据库类型
+    // 'DB_HOST'   => 'localhost', // 服务器地址
+    // 'DB_NAME'   => 'wodeshujuku', // 数据库名
+    // 'DB_USER'   => 'root', // 用户名
+    // 'DB_PWD'    => '',  // 密码
+    // 'DB_PORT'   => '3306', // 端口
+    // 'DB_PREFIX' => 'bug_', // 数据库表前缀
+
+	// 'TMPL_PARSE_STRING'  =>array(
+	// 	'__PUBLIC__' => '/assets', // 更改默认的/Public 替换规则
+	// 	'__JS__'     => '/assets/JS/', // 增加新的JS类库路径替换规则
+	// 	'__UPLOAD__' => '/assets/Uploads', // 增加新的上传路径替换规则
+	// ),
+
+	'DEFAULT_MODULE' => 'Pickle',
+	'SHOW_PAGE_TRACE' => false,
+
+);
